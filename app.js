251c6a70b0909ff94c16e90f67eb53342e0275d9
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2021-08-17 08:38:45 -0400

    Create some components

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
+app.set("views", "views")
 
-app.get("*", (req, res) => res.render("home"))
+app.get("/", (req, res) => res.render("home"));
+app.get("/cake-duty", (req, res) => res.render("cake-duty"));
 
 app.listen(3000, () => console.log("Up and rolling!"));
